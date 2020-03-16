4c46eb83ceac997e73aa4a527c72a7eb08138978
Anh Tuan Truong <atruon16@student.aau.dk>
2020-03-16 11:55:03 +0100

    Added different projects

---
+++ P4TableManagement/TableManagementConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TableManagementConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
