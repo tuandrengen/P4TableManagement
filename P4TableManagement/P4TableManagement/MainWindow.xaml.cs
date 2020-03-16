e0b616ffa0032bb63cddcd39327f3016cc1c91aa
Tommy Pham <tphazs@gmail.com>
2020-03-16 12:11:12 +0100

    +test234

--- P4TableManagement/P4TableManagement/MainWindow.xaml.cs
+++ P4TableManagement/P4TableManagement/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace P4TableManagement
         public MainWindow()
         {
             InitializeComponent();
+            Console.WriteLine("Test123 test 123");
+            Console.WriteLine("test234 234");
         }
     }
 }
