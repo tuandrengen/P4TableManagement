b2c287951a6494155c00e28778ada4ac9b3ecdcf
Tommy Pham <tphazs@gmail.com>
2020-03-23 10:32:49 +0100

    Excel virker

--- P4TableManagement/TableManagementConsole/Program.cs
+++ P4TableManagement/TableManagementConsole/Program.cs
@@ -6,7 +6,7 @@ namespace TableManagementConsole
     {
         static void Main(string[] args)
         {
-            Excel test = new Excel(@"C:\Users\123\Desktop\test\test.xlsx", 1);
+            Excel test = new Excel(@"C:\Users\T-Phamz\Desktop\test\test.xlsx", 1);
             string res = test.ReadCell(0 , 0);
             Console.WriteLine(res);
         }
