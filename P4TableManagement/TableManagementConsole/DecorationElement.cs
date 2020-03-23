d4efe37be71aedf5e695ddb65006c6588c3954c7
Casper <World@DESKTOP-TTS38P3>
2020-03-23 13:18:37 +0100

    decoration element metode "CreateDecorationElement" med dummy data i Program.

--- P4TableManagement/TableManagementConsole/DecorationElement.cs
+++ P4TableManagement/TableManagementConsole/DecorationElement.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace TableManagementConsole
 {
-    class DecorationElement : MapElement
+    public class DecorationElement : MapElement
     {
         private int _zoneWidth;
         private int _zoneHeight;
@@ -29,6 +30,8 @@ namespace TableManagementConsole
 			set { _name = value; }
 		}
 
+        public static List<DecorationElement> decorationElementList = new List<DecorationElement>();
+
         //constructor
 		public DecorationElement(string name, int zoneWidth, int zoneHeight, int width, int height, int placementX, int placementY) : base(width, height, placementX, placementY)
 		{
@@ -36,5 +39,41 @@ namespace TableManagementConsole
             this.zoneWidth = zoneWidth;
             this.zoneHeight = zoneHeight;
 		}
+
+        // We create a new DecorationElement and adds it to the overall list (redundant parameters?)
+        public void CreateDecorationElement(DecorationElement newDE)
+        {
+            bool listValid = false;
+
+            // Kontrolstruktur der tjekker om elementet allerede eksisterer i listen
+            foreach (var item in decorationElementList)
+            {
+                Console.WriteLine($"Vi tjekker dette item {item.name}");
+                if (item.name == newDE.name)
+                {
+                    // De to elementer er ens og derfor kan den ikke sættes ind i listen.
+                    // Man kunne prompte brugeren til at ændre navnet på newDE
+                    Console.WriteLine($"ERROR - Der eksisterer allerede et {item.name}");
+                    Console.WriteLine();
+                    listValid = false;
+                    break;
+                }
+                else
+                {
+                    listValid = true;
+                }
+                Console.WriteLine();
+            }
+            if (listValid)
+            {
+                Console.WriteLine("Der eksisterer ikke en lignende, så vi tilføjer");
+                decorationElementList.Add(newDE);
+            }
+        }
+
+        public void DeleteDecorationElement(DecorationElement DE)
+        {
+            decorationElementList.Remove(DE);
+        }
     }
 }
--- P4TableManagement/TableManagementConsole/Program.cs
+++ P4TableManagement/TableManagementConsole/Program.cs
@@ -8,23 +8,39 @@ namespace TableManagementConsole
     {
         static void Main(string[] args)
         {
-            Table table1 = new SmallTable() { tableNumber = 1 };
-            Table table2 = new LargeTable() { tableNumber = 2 };
-            Table table3 = new SmallTable() { tableNumber = 3 };
-            Table table4 = new SmallTable() { tableNumber = 4 };
+            //Table table1 = new SmallTable() { tableNumber = 1 };
+            //Table table2 = new LargeTable() { tableNumber = 2 };
+            //Table table3 = new SmallTable() { tableNumber = 3 };
+            //Table table4 = new SmallTable() { tableNumber = 4 };
 
-            Table table5 = new CombinedTable<Table>(table1, table3);
+            //Table table5 = new CombinedTable<Table>(table1, table3);
 
-            Table.tableList.Add(table1);
-            Table.tableList.Add(table2);
-            Table.tableList.Add(table3);
-            Table.tableList.Add(table4);
-            Table.tableList.Add(table5);
+            //Table.tableList.Add(table1);
+            //Table.tableList.Add(table2);
+            //Table.tableList.Add(table3);
+            //Table.tableList.Add(table4);
+            //Table.tableList.Add(table5);
 
-            PrintList(Table.tableList);
+            //PrintList(Table.tableList);
 
-            Table.DeleteTableFromList(3);
-            PrintList(Table.tableList);
+            //Table.DeleteTableFromList(3);
+            //PrintList(Table.tableList);
+
+            PopulateList(DecorationElement.decorationElementList);
+            PrintList(DecorationElement.decorationElementList);
+
+            DecorationElement Bar = new DecorationElement("Bar", 2, 2, 2, 2, 2, 2);
+            DecorationElement Bar2 = new DecorationElement("Bar2", 2, 2, 2, 2, 2, 2);
+            DecorationElement Aquarium = new DecorationElement("Aquarium", 2, 2, 2, 2, 2, 2);
+
+            Bar.CreateDecorationElement(Bar);
+            Bar2.CreateDecorationElement(Bar2);
+            Aquarium.CreateDecorationElement(Aquarium);
+
+            PrintList(DecorationElement.decorationElementList);
+
+
+
 
         }
 
@@ -37,6 +53,30 @@ namespace TableManagementConsole
             Console.WriteLine();
         }
 
+        public static void PrintList(List<DecorationElement> l)
+        {
+            foreach (DecorationElement item in l)
+            {
+                Console.WriteLine($"Name: {item.name}");
+            }
+            Console.WriteLine();
+        }
+
+        public static void PopulateList(List<DecorationElement> l)
+        {
+            DecorationElement test1 = new DecorationElement("Bar", 2, 2, 2, 2, 2, 2);
+            DecorationElement test2 = new DecorationElement("Casino", 2, 2, 2, 2, 2, 2);
+            DecorationElement test3 = new DecorationElement("Window", 2, 2, 2, 2, 2, 2);
+            DecorationElement test4 = new DecorationElement("Aquarium", 2, 2, 2, 2, 2, 2);
+            DecorationElement test5 = new DecorationElement("Window2", 2, 2, 2, 2, 2, 2);
+
+            l.Add(test1);
+            l.Add(test2);
+            l.Add(test3);
+            l.Add(test4);
+            l.Add(test5);
+
+        }
 
         /*
          * TODO:
@@ -49,8 +89,9 @@ namespace TableManagementConsole
          *      to the list of tables.
          * 
          * 2)   DecorationElement.cs and MapElement.cs
-         * 2.a) Find out what kinds of decoration elements there are
-         *      e.g. Aquarium, playroom, etc.
+         * 2.a) Find out what kinds of decoration elements there are (all elements which creates a zone)
+         *      e.g. Aquarium, playroom, Bar, Window etc.
+         *      
          * 2.b) Does DecorationElement.cs and MapElement.cs work?
          *      If not, make them.
          *      
