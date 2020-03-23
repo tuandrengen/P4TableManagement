47ecfaed15a2675e64768726f338bd0ab3dfce3b
Tommy Pham <tphazs@gmail.com>
2020-03-23 13:24:34 +0100

    Revert test 2

--- P4TableManagement/TableManagementConsole/ReservationList.cs
+++ P4TableManagement/TableManagementConsole/ReservationList.cs
@@ -26,9 +26,11 @@ namespace TableManagementConsole
 
 
         //indtager data fra databasen, laver daten om til objekter(reservations) og putter dem i en liste
-        public void PopulateReservationList()
+        public void PopulateReservationList(string path, int sheet)
         {
-             
+            //Excel ReservationList = new Excel(path, sheet);
+            //List<string> res = ReservationList.ReadCell();
+            //res.ForEach(Console.WriteLine);
         }
 
         //sletter reservationer
