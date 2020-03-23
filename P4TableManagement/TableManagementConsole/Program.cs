125b5524b56f560ceab0c644350476ecf27854ac
Yongxin Zhang <yzhan18@student.aau.dk>
2020-03-23 21:00:31 +0800

    Nu boer det virker

--- P4TableManagement/TableManagementConsole/Program.cs
+++ P4TableManagement/TableManagementConsole/Program.cs
@@ -7,12 +7,8 @@ namespace TableManagementConsole
     {
         static void Main(string[] args)
         {
-            Excel test = new Excel(@"C:\Users\123\Desktop\test\test.xlsx", 1);
-            List<string> res = test.ReadCell();
-            foreach (string item in res)
-            {
-                Console.WriteLine(item);
-            }
+            ReservationList list = new ReservationList();
+            list.PopulateReservationList(@"C:\Users\T-Phamz\Desktop\test\test.xlsx", 1);
         }
     }
 }
--- P4TableManagement/TableManagementConsole/ReservationList.cs
+++ P4TableManagement/TableManagementConsole/ReservationList.cs
@@ -26,9 +26,14 @@ namespace TableManagementConsole
 
 
         //indtager data fra databasen, laver daten om til objekter(reservations) og putter dem i en liste
-        public void PopulateReservationList()
+        public void PopulateReservationList(string path, int sheet)
         {
-             
+            Excel test = new Excel(path, sheet);
+            List<Reservation> list = test.ReadCell();
+            foreach (var reservation in list)
+            {
+                Console.WriteLine($"{reservation.name}, {reservation.numberOfGuests}, {reservation.phoneNumber}, {reservation.timeStart.ToString("HH:mm")}, {reservation.comment}, {string.Join(", ", reservation.parameter)}");
+            }
         }
 
         //sletter reservationer
