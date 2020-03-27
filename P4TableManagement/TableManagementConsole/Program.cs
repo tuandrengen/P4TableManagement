c9c5e7b16522cf0dbe6ff92859291c860ab8185d
Tommy Pham <tphazs@gmail.com>
2020-03-27 13:26:34 +0100

    Sortlist With testing Success

--- P4TableManagement/TableManagementConsole/Program.cs
+++ P4TableManagement/TableManagementConsole/Program.cs
@@ -8,7 +8,9 @@ namespace TableManagementConsole
         static void Main(string[] args)
         {
             ReservationList list = new ReservationList();
-            list.PopulateReservationList(@"C:\Users\T-Phamz\Desktop\test\test.xlsx", 1);
+            //list.PopulateReservationList(@"C:\Users\T-Phamz\Desktop\test\test.xlsx", 1);
+            //list.DeleteReservations(@"C:\Users\T-Phamz\Desktop\test\test.xlsx", 1, 8);
+            //list.EditReservations(@"C:\Users\T-Phamz\Desktop\test\test.xlsx", 1, 1, 2);
         }
     }
 }
