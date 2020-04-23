2a22d8059155481a0f716866d517007f65cf4ab2
Anh Tuan Truong <atruon16@student.aau.dk>
2020-04-23 10:24:40 +0200

    Moved and refactored code from Table.cs to TableManagementSystem.cs - where all the operations are going to be for the system.

--- P4TableManagement/TableManagementConsole/Table.cs
+++ P4TableManagement/TableManagementConsole/Table.cs
@@ -1,21 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
 
 namespace TableManagementConsole
 {
-    public abstract class Table : MapElement, IComparable<Table>
+	public abstract class Table : MapElement, IComparable<Table>
     {
+		private static int _tableID = 0;
 		public int seats { get; set; }
 		// tableNumber has been changed from private set; to protected set; 
 		// as sub classes should be able to set this value as well
-		public int tableNumber { get; set; }
+		public int tableNumber { get; private set; }
 		public int bookingID { get; set; }
 		public string state { get; set; }
-
-		private static int _tableID = 0;
 
 		public int ID { get; set; }
 
@@ -30,78 +27,6 @@ namespace TableManagementConsole
 			// https://stackoverflow.com/questions/8813435/incrementing-a-unique-id-number-in-the-constructor
 			ID = System.Threading.Interlocked.Increment(ref _tableID);
 			state = "Available";
-		}
-
-		public void AddTableToList(List<Table> tableList)
-		{
-			tableList.Add(this);
-		}
-
-		public void DeleteTableFromList(List<Table> tableList)
-		{
-			tableList.Remove(this);
-			//// .ToList and Linq library
-			//// https://stackoverflow.com/questions/604831/collection-was-modified-enumeration-operation-may-not-execute
-			//foreach (Table table in tableList.ToList())
-			//{
-			//	if (table.ID == id)
-			//	{
-			//		tableList.Remove(item);
-			//	}
-			//}
-		}
-
-		public void UnassignTable()
-		{
-			bookingID = default;
-			state = "Available";
-			Console.WriteLine($"Table #{ tableNumber } has been unassigned!");
-		}
-
-		public void PayTable()
-		{
-			state = "Paid";
-			Console.WriteLine($"Table #{ tableNumber } has been paid! Booking ID: { bookingID }");
-		}
-
-		public void ReserveTable(Booking booking)
-		{
-			bookingID = booking.id;
-			state = "Reserved";
-			Console.WriteLine($"Table #{ tableNumber } has been reserved! Booking ID: { bookingID }");
-		}
-
-		public static List<Table> GetTableList(Predicate<Table> searchCriteria, List<Table> tableList)
-		{
-			List<Table> newList = tableList.FindAll(searchCriteria);
-			return newList;
-		}
-
-		// Combines this object of a table with another table by deleting the two
-		// tables from the tableList and then returning the new CombinedTable.
-		public Table CombineTables(Table other, List<Table> tableList)
-		{
-			foreach (Table table in tableList.ToList())
-			{
-				if (table.tableNumber == this.tableNumber || table.tableNumber == other.tableNumber)
-				{
-					tableList.Remove(table);
-				}
-			}
-
-			return new CombinedTable<Table>(this, other);
-		}
-
-		// Separates table by adding the two tables that were combined at first,
-		// and adding them back to tableList.
-		public static void SeparateTables(CombinedTable<Table> inputTable, List<Table> tableList)
-		{
-			foreach (Table table in inputTable.combinedTables)
-			{
-				tableList.Add(table);
-			}
-
-			tableList.Remove(inputTable);
 		}
 
 		public int CompareTo([AllowNull] Table other)
