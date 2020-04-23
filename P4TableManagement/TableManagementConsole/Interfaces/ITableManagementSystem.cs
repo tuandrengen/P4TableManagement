dbc2e206ed864b2ce5eb8d02fbf2356c72cc9f16
Anh Tuan Truong <atruon16@student.aau.dk>
2020-04-23 10:27:49 +0200

    Added exception and Interface

---
+++ P4TableManagement/TableManagementConsole/Interfaces/ITableManagementSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TableManagementConsole
+{
+    public interface ITableManagementSystem
+    {
+        List<Table> TableList { get; }
+        void AddTableToList(Table table);
+        void AssignTable(Table table, Booking booking);
+        Table CombineTables(Table one, Table two);
+        void DeleteTableFromList(Table table);
+        List<Table> GetTableList(Predicate<Table> searchCriteria);
+        void PayTable(Table table);
+        void ReserveTable(Table table, Booking booking);
+        void SeparateTables(CombinedTable<Table> combinedTable);
+        void UnassignTable(Table table);
+    }
+}
