bc4bc9fa93ebd7cea3cba06b5e53f3d4c846623f
Casper <World@DESKTOP-TTS38P3>
2020-04-24 09:53:59 +0200

    Interactable rectangles

--- P4TableManagement/P4TableManagement/MainWindow.xaml.cs
+++ P4TableManagement/P4TableManagement/MainWindow.xaml.cs
@@ -24,15 +24,19 @@ namespace P4TableManagement
         {
             InitializeComponent();
 
-            List<Cell> items = new List<Cell>();
-            items.Add(new Cell() { X = 69, Y = 42, Z = 420});
+            //List<Cell> items = new List<Cell>();
+            //items.Add(new Cell() { X = 69, Y = 42, Z = 420});
 
-            lvCells.ItemsSource = items;
+            ////lvCells.ItemsSource = items;
+            
 
+            
+
+            lvCells.ItemsSource = AllRectangles;
         }
 
         public class Cell
-        {
+        {            
             public int X { get; set; }
 
             public int Y { get; set; }
@@ -44,9 +48,11 @@ namespace P4TableManagement
         private void Window_ContentRendered(object sender, EventArgs e)
         {
             DrawGameArea();
+            PopulateRectanglesList();
         }
 
         const int SquareSize = 50;
+        public List<Rectangle> AllRectangles = new List<Rectangle>();
 
         private void DrawGameArea()
         {
@@ -65,14 +71,17 @@ namespace P4TableManagement
                 //    Stroke = Brushes.Black
                 //};
 
-                Button rect = new Button
+                Rectangle rect = new Rectangle
                 {
                     Width = SquareSize,
-                    Height = SquareSize
-
+                    Height = SquareSize,
+                    Fill = Brushes.White,
+                    Stroke = Brushes.Red
                 };
+                //rect.Fill = new SolidColorBrush(Colors.White);
 
                 Area.Children.Add(rect);
+                AllRectangles.Add(rect);
                 Canvas.SetTop(rect, nextY);
                 Canvas.SetLeft(rect, nextX);
 
@@ -93,6 +102,111 @@ namespace P4TableManagement
 
 
 
+
+
+        // The drag's last point.
+        private Point LastPoint;
+
+        // The part of the rectangle the mouse is over.
+        private enum HitType
+        {
+            None, Body, UL, UR, LR, LL, L, R, T, B
+        };
+
+        // The part of the rectangle under the mouse.
+        private HitType MouseHitType = HitType.None;
+
+        // The Rectangle that was hit.
+        private Rectangle HitRectangle = null;
+
+        // The Rectangles that the user can move and resize.
+        private List<Rectangle> Rectangles = new List<Rectangle>();
+
+        private void PopulateRectanglesList()
+        {
+            foreach (UIElement child in Area.Children)
+            {
+                if (child is Rectangle)
+                    Rectangles.Add(child as Rectangle);
+            }
+
+            // Reverse the list so the Rectangles on top come first.
+            Rectangles.Reverse();
+        }
+
+        // If the point is over any Rectangle,
+        // return the Rectangle and the hit type.
+        private void FindHit(Point point)
+        {
+            HitRectangle = null;
+            MouseHitType = HitType.None;
+
+            foreach (Rectangle rect in AllRectangles)
+            {
+                MouseHitType = SetHitType(rect, point);
+                if (MouseHitType != HitType.None)
+                {
+                    HitRectangle = rect;
+                    return;
+                }
+            }
+
+            // We didn't find a hit.
+            return;
+        }
+
+        // Return a HitType value to indicate what is at the point.
+        private HitType SetHitType(Rectangle rect, Point point)
+        {
+            double left = Canvas.GetLeft(rect);
+            double top = Canvas.GetTop(rect);
+            double right = left + rect.Width;
+            double bottom = top + rect.Height;
+            if (point.X < left) return HitType.None;
+            if (point.X > right) return HitType.None;
+            if (point.Y < top) return HitType.None;
+            if (point.Y > bottom) return HitType.None;
+
+            const double GAP = 10;
+            if (point.X - left < GAP)
+            {
+                // Left edge.
+                if (point.Y - top < GAP) return HitType.UL;
+                if (bottom - point.Y < GAP) return HitType.LL;
+                return HitType.L;
+            }
+            if (right - point.X < GAP)
+            {
+                // Right edge.
+                if (point.Y - top < GAP) return HitType.UR;
+                if (bottom - point.Y < GAP) return HitType.LR;
+                return HitType.R;
+            }
+            if (point.Y - top < GAP) return HitType.T;
+            if (bottom - point.Y < GAP) return HitType.B;
+            return HitType.Body;
+        }
+
+        // Start dragging.
+        private void Area_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            FindHit(Mouse.GetPosition(Area));
+            //SetMouseCursor();
+            if (MouseHitType == HitType.None) return;
+
+            LastPoint = Mouse.GetPosition(Area);
+            //DragInProgress = true;
+
+            if (HitRectangle.Fill == Brushes.White)
+            {
+                HitRectangle.Fill = Brushes.Red;
+            }
+            else
+            {
+                HitRectangle.Fill = Brushes.White;
+            }
+
+        }
 
 
 
