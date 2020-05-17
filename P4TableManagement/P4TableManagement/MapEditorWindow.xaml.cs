26787e61d66f5bf4d516104f5b252dd35f05ed94
Casper <World@DESKTOP-TTS38P3>
2020-05-17 15:33:16 +0200

    New window: MapSectionEditorWindow. This is the window where the user can edit the selected map section or create a completely new one

---
+++ P4TableManagement/P4TableManagement/MapEditorWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P4TableManagement
+{
+    /// <summary>
+    /// Interaction logic for MapEditorWindow.xaml
+    /// </summary>
+    public partial class MapEditorWindow : Window
+    {
+        public MapEditorWindow()
+        {
+            InitializeComponent();
+
+            //Draw map sections
+            DrawCanvas();
+            // ...
+
+        }
+
+        private void DrawCanvas()
+        {
+            // Vi skal tegne alle eksisterende map sections (indlæst fra en local? fil)
+            // En map section tegnes med en rektangel/knap med et navn f.eks. Section 1.
+            // CreateMapSection skal have sin egen knap som bliver en knap med et plus icon i midten.
+
+            int nextX = 0, nextY = 0;
+            int rowCounter = 0;
+            //int startXY = 10;
+            int squareSize = 100;
+            bool doneDrawing = false;
+            int Margin = 10;
+
+
+            // Først skal der tegnes de eksisterende map sections, derefter kommer CreateMapSection knappen.
+            //while (!doneDrawing)
+            //{
+                // Draw map sections from file
+                // create button for each map section yada yada
+
+                // Makes sure we don't continue drawing to the right when we have reached the end of the canvas
+                // resets the nextX value so we draw 
+                //if (nextX >= MapSectionsCanvas.ActualWidth)
+                //{
+                //    nextX = Margin;
+                //    nextY += squareSize;
+                //}
+                // When we have drawn a button for each map section that exists from the given file, then we stop.
+                //if (end of line = true)
+                //{
+                //  doneDrawing = true;
+                //}
+            //}
+
+
+            // We make the "plus icon" button
+            Button button = new Button()
+            {
+                Width = squareSize,
+                Height = squareSize,
+                Background = Brushes.White,
+                Content = "plus icon" // Vi skal implementere et billede her af et plus icon ._.
+            };
+
+            // Sets position
+            Canvas.SetTop(button, nextY + Margin);
+            Canvas.SetLeft(button, nextX + Margin);
+
+            // Adds it to the Canvas
+            MapSectionsCanvas.Children.Add(button);
+
+            // Subscribes the click event
+            button.Click += new RoutedEventHandler(CreateMapSection);
+
+            
+
+
+
+
+        } //end of DrawCanvas
+
+        // Event for creating a new map section
+        private void CreateMapSection(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
